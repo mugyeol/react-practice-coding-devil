59b423a34c95c54e48ba69b75075207bc0a97ac2
mugyeol <htpadkorik@naver.com>
2022-07-13 21:29:17 +0900

    220713

---
+++ react_practice_02/src/component/Header.js
@@ -0,0 +1,16 @@
+export default function Header(){
+    return (
+        <div>
+            <h1>
+            <a href="/">토익 영단어(고급)</a>
+            </h1>
+        <div className="menu"></div>
+        <a href="#x" className="link">
+            단어 추가
+        </a>
+        <a href="#x" className="link">
+            단어 추가
+        </a>
+        </div>
+    );
+}
